c629800b6dc1a39f93a4eb1b89ace37f5d24f9a8
Jonathan He <jonathanhe008@gmail.com>
2023-01-27 17:03:09 -0800

    Attempt to fix json error 2

--- headshot.js
+++ headshot.js
@@ -1,13 +1,20 @@
-import players from './players.json' assert { type: "json" };;
 export async function fetchHeadshot(name) {
-    for (let player of players['league']['standard']) {
-        if (`${player['firstName']} ${player['lastName']}` == name) {
-            const img = document.querySelector("#headshot");
-            img.src = `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${player['personId']}.png`;
-            return player['personId'];
+    fetch("./players.json", {
+        headers: {
+            'Content-Type': 'application/json'
         }
-    }
-    const img = document.querySelector("#headshot");
-    img.src = `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/2544.png`;
-    return 2544;
+    })
+    .then(response => response.json())
+    .then(players => {
+        for (let player of players['league']['standard']) {
+            if (`${player['firstName']} ${player['lastName']}` == name) {
+                const img = document.querySelector("#headshot");
+                img.src = `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${player['personId']}.png`;
+                return player['personId'];
+            }
+        }
+        const img = document.querySelector("#headshot");
+        img.src = `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/2544.png`;
+        return 2544;
+    });
 }
