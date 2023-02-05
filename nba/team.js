1cd3215bb64bca94026c8a073e57c1a741c6ad5f
Jonathan He <jonathanhe008@gmail.com>
2023-02-04 20:55:12 -0800

    Add team table and remove solid outline

--- nba/team.js
+++ nba/team.js
@@ -27,14 +27,42 @@ export async function fetchSeasonAverage(players) {
 
     console.log("PlayerDict => ", player_dict);
 
+    var table = "<table class='table'>" +
+            "<thead>" +
+            "<tr>" +
+            "<th>Player</th>" +
+            "<th>Pts</th>" +
+            "<th>Ast</th>" +
+            "<th>Reb</th>" +
+            "<th>Stl</th>" +
+            "<th>Blk</th>" +
+            "<th>Tov</th>" +
+            "</tr>" +
+            "</thead>" +
+            "<tbody>";
+
     for (let averages of data.data) {
         let player = player_dict[averages['player_id']];
         var stats = ["pts", "ast", "reb", "blk", "stl", "turnover"];
         stats.forEach(function(stat) {
             totals_map[stat][`${player['firstName']} ${player['lastName']}`] = Math.ceil(averages[stat] * averages['games_played']);
         });
+
+        table += "<tr>" +
+             "<td> <img src=\"" + `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${player['personId']}.png` + "\" class=\"img-fluid img-max-size\"></img> " 
+             + `${player['firstName']} ${player['lastName']}` + "</td>" +
+             "<td>" + totals_map['pts'][`${player['firstName']} ${player['lastName']}`] + "</td>" +
+             "<td>" + totals_map['ast'][`${player['firstName']} ${player['lastName']}`] + "</td>" +
+             "<td>" + totals_map['reb'][`${player['firstName']} ${player['lastName']}`] + "</td>" +
+             "<td>" + totals_map['stl'][`${player['firstName']} ${player['lastName']}`] + "</td>" +
+             "<td>" + totals_map['blk'][`${player['firstName']} ${player['lastName']}`] + "</td>" +
+             "<td>" + totals_map['turnover'][`${player['firstName']} ${player['lastName']}`]+ "</td>" +
+             "</tr>";
     }
 
+    table += "</tbody>" +
+         "</table>";
+    document.getElementById("table_container").innerHTML = table;
     console.log("team_totals_map => ", totals_map);
     return totals_map;
     
