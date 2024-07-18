36871876f10fd5f3e3c16d1b6ef3c01caf7bedd6
Jonathan He <jonathanhe008@gmail.com>
2024-07-17 22:57:41 -0400

    Remove nba files

--- nba/player.js
+++
@@ -1,9 +0,0 @@
-export async function fetchPlayer(apiId) {
-    var url = new URL(`https://www.balldontlie.io/api/v1/players/${apiId}`);
-    let response = await fetch(url, { method: "GET" });
-    let data = await response.json();
-
-    console.log("fetchPlayer => ", data);
-
-    return data;
-}
